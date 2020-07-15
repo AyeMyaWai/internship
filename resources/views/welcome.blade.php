eec40a4fbe2f2a699f09a8cbd7c118b18775a92c
AyeMyaWai <61411011+AyeMyaWai@users.noreply.github.com>
2020-07-15 11:17:30 +0630

    login design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,104 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+<!--A Design by W3layouts
+   Author: W3layout
+   Author URL: http://w3layouts.com
+   License: Creative Commons Attribution 3.0 Unported
+   License URL: http://creativecommons.org/licenses/by/3.0/
+   -->
+   <!DOCTYPE html>
+<html lang="en">
+   <head>
+      <title>Gaze Sign up & login Form a Flat Responsive Widget Template :: w3layouts </title>
+      <!-- Meta tags -->
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="keywords" content="Gaze Sign up & login Form Responsive Widget, Audio and Video players, Login Form Web Template, Flat Pricing Tables, Flat Drop-Downs, Sign-Up Web Templates, Flat Web Templates, Login Sign-up Responsive Web Template, Smartphone Compatible Web Template, Free Web Designs for Nokia, Samsung, LG, Sony Ericsson, Motorola Web Design"
+         />
+      <script>
+         addEventListener("load", function () { setTimeout(hideURLbar, 0); }, false); function hideURLbar() { window.scrollTo(0, 1); }
+      </script>
+      <!-- Meta tags -->
+      <!--stylesheets-->
+      <link href="{{asset('css/style.css')}}" rel='stylesheet' type='text/css' media="all">
+      <!--//style sheet end here-->
+      <link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700" rel="stylesheet">
+   </head>
+   <body>
+      <div class="mid-class">
+         <div class="art-right-w3ls">
+            <h2>Sign In and Sign Up</h2>
+            <form action="#" method="post">
+               <div class="main">
+                  <div class="form-left-to-w3l">
+                     <input type="text" name="name" placeholder="Username" required="">
+                  </div>
+                  <div class="form-left-to-w3l ">
+                     <input type="password" name="password" placeholder="Password" required="">
+                     <div class="clear"></div>
+                  </div>
+               </div>
+               <div class="left-side-forget">
+                  <input type="checkbox" class="checked">
+                  <span class="remenber-me">Remember me </span>
+               </div>
+               <div class="right-side-forget">
+                  <a href="#" class="for">Forgot password...?</a>
+               </div>
+               <div class="clear"></div>
+               <div class="btnn">
+                  <button type="submit">Sign In</button>
+               </div>
+            </form>
+            <div class="w3layouts_more-buttn">
+               <h3>Don't Have an account..?
+                  <a href="#content1">Sign Up Here
+                  </a>
+               </h3>
             </div>
-        </div>
-    </body>
+            <!-- popup-->
+            <div id="content1" class="popup-effect">
+               <div class="popup">
+                  <!--login form-->
+                  <div class="letter-w3ls">
+                     <form action="#" method="post">
+                        <div class="form-left-to-w3l">
+                           <input type="text" name="name" placeholder="Username" required="">
+                        </div>
+                        <div class="form-left-to-w3l">
+                           <input type="text" name="name" placeholder="Phone" required="">
+                        </div>
+                        <div class="form-left-to-w3l">
+                           <input type="email" name="email" placeholder="Email" required="">
+                        </div>
+                        <div class="form-left-to-w3l">
+                           <input type="password" name="password" placeholder="Password" required="">
+                        </div>
+                        <div class="form-left-to-w3l margin-zero">
+                           <input type="password" name="password" placeholder="Confirm Password" required="">
+                        </div>
+                        <div class="btnn">
+                           <button type="submit">Sign Up</button>
+                           <br>
+                        </div>
+                     </form>
+                     <div class="clear"></div>
+                  </div>
+                  <!--//login form-->
+                  <a class="close" href="#">&times;</a>
+               </div>
+            </div>
+            <!-- //popup -->
+         </div>
+         <div class="art-left-w3ls">
+            <h1 class="header-w3ls">
+               Gaze sign up & login Form
+            </h1>
+         </div>
+      </div>
+      <footer class="bottem-wthree-footer">
+         <p>
+            © 2019 Gaze Sign Up & Login Form. All Rights Reserved | Design by
+            <a href="http://www.W3Layouts.com" target="_blank">W3Layouts</a>
+         </p>
+      </footer>
+   </body>
 </html>
