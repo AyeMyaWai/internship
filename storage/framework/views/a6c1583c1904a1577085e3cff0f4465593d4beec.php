6354e0818dac5eadea350232564d594e1cb67d6c
eithinzarhtet <capital.eithinzarhtet@gmail.com>
2020-07-16 08:47:35 +0630

    admin template

--- storage/framework/views/a6c1583c1904a1577085e3cff0f4465593d4beec.php
+++ storage/framework/views/a6c1583c1904a1577085e3cff0f4465593d4beec.php
@@ -1,104 +1,92 @@
-<!--A Design by W3layouts
-   Author: W3layout
-   Author URL: http://w3layouts.com
-   License: Creative Commons Attribution 3.0 Unported
-   License URL: http://creativecommons.org/licenses/by/3.0/
-   -->
-   <!DOCTYPE html>
-<html lang="en">
-   <head>
-      <title>Gaze Sign up & login Form a Flat Responsive Widget Template :: w3layouts </title>
-      <!-- Meta tags -->
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-      <meta name="keywords" content="Gaze Sign up & login Form Responsive Widget, Audio and Video players, Login Form Web Template, Flat Pricing Tables, Flat Drop-Downs, Sign-Up Web Templates, Flat Web Templates, Login Sign-up Responsive Web Template, Smartphone Compatible Web Template, Free Web Designs for Nokia, Samsung, LG, Sony Ericsson, Motorola Web Design"
-         />
-      <script>
-         addEventListener("load", function () { setTimeout(hideURLbar, 0); }, false); function hideURLbar() { window.scrollTo(0, 1); }
-      </script>
-      <!-- Meta tags -->
-      <!--stylesheets-->
-      <link href="<?php echo e(asset('css/style.css')); ?>" rel='stylesheet' type='text/css' media="all">
-      <!--//style sheet end here-->
-      <link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700" rel="stylesheet">
-   </head>
-   <body>
-      <div class="mid-class">
-         <div class="art-right-w3ls">
-            <h2>Sign In and Sign Up</h2>
-            <form action="#" method="post">
-               <div class="main">
-                  <div class="form-left-to-w3l">
-                     <input type="text" name="name" placeholder="Username" required="">
-                  </div>
-                  <div class="form-left-to-w3l ">
-                     <input type="password" name="password" placeholder="Password" required="">
-                     <div class="clear"></div>
-                  </div>
-               </div>
-               <div class="left-side-forget">
-                  <input type="checkbox" class="checked">
-                  <span class="remenber-me">Remember me </span>
-               </div>
-               <div class="right-side-forget">
-                  <a href="#" class="for">Forgot password...?</a>
-               </div>
-               <div class="clear"></div>
-               <div class="btnn">
-                  <button type="submit">Sign In</button>
-               </div>
-            </form>
-            <div class="w3layouts_more-buttn">
-               <h3>Don't Have an account..?
-                  <a href="#content1">Sign Up Here
-                  </a>
-               </h3>
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header"><?php echo e(__('Login')); ?></div>
+
+                <div class="card-body">
+                    <form method="POST" action="<?php echo e(route('login')); ?>">
+                        <?php echo csrf_field(); ?>
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right"><?php echo e(__('E-Mail Address')); ?></label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control <?php if ($errors->has('email')) :
+if (isset($message)) { $messageCache = $message; }
+$message = $errors->first('email'); ?> is-invalid <?php unset($message);
+if (isset($messageCache)) { $message = $messageCache; }
+endif; ?>" name="email" value="<?php echo e(old('email')); ?>" required autocomplete="email" autofocus>
+
+                                <?php if ($errors->has('email')) :
+if (isset($message)) { $messageCache = $message; }
+$message = $errors->first('email'); ?>
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong><?php echo e($message); ?></strong>
+                                    </span>
+                                <?php unset($message);
+if (isset($messageCache)) { $message = $messageCache; }
+endif; ?>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right"><?php echo e(__('Password')); ?></label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control <?php if ($errors->has('password')) :
+if (isset($message)) { $messageCache = $message; }
+$message = $errors->first('password'); ?> is-invalid <?php unset($message);
+if (isset($messageCache)) { $message = $messageCache; }
+endif; ?>" name="password" required autocomplete="current-password">
+
+                                <?php if ($errors->has('password')) :
+if (isset($message)) { $messageCache = $message; }
+$message = $errors->first('password'); ?>
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong><?php echo e($message); ?></strong>
+                                    </span>
+                                <?php unset($message);
+if (isset($messageCache)) { $message = $messageCache; }
+endif; ?>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" <?php echo e(old('remember') ? 'checked' : ''); ?>>
+
+                                    <label class="form-check-label" for="remember">
+                                        <?php echo e(__('Remember Me')); ?>
+
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    <?php echo e(__('Login')); ?>
+
+                                </button>
+
+                                <?php if(Route::has('password.request')): ?>
+                                    <a class="btn btn-link" href="<?php echo e(route('password.request')); ?>">
+                                        <?php echo e(__('Forgot Your Password?')); ?>
+
+                                    </a>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <!-- popup-->
-            <div id="content1" class="popup-effect">
-               <div class="popup">
-                  <!--login form-->
-                  <div class="letter-w3ls">
-                     <form action="#" method="post">
-                        <div class="form-left-to-w3l">
-                           <input type="text" name="name" placeholder="Username" required="">
-                        </div>
-                        <div class="form-left-to-w3l">
-                           <input type="text" name="name" placeholder="Phone" required="">
-                        </div>
-                        <div class="form-left-to-w3l">
-                           <input type="email" name="email" placeholder="Email" required="">
-                        </div>
-                        <div class="form-left-to-w3l">
-                           <input type="password" name="password" placeholder="Password" required="">
-                        </div>
-                        <div class="form-left-to-w3l margin-zero">
-                           <input type="password" name="password" placeholder="Confirm Password" required="">
-                        </div>
-                        <div class="btnn">
-                           <button type="submit">Sign Up</button>
-                           <br>
-                        </div>
-                     </form>
-                     <div class="clear"></div>
-                  </div>
-                  <!--//login form-->
-                  <a class="close" href="#">&times;</a>
-               </div>
-            </div>
-            <!-- //popup -->
-         </div>
-         <div class="art-left-w3ls">
-            <h1 class="header-w3ls">
-               Gaze sign up & login Form
-            </h1>
-         </div>
-      </div>
-      <footer class="bottem-wthree-footer">
-         <p>
-            © 2019 Gaze Sign Up & Login Form. All Rights Reserved | Design by
-            <a href="http://www.W3Layouts.com" target="_blank">W3Layouts</a>
-         </p>
-      </footer>
-   </body>
-</html><?php /**PATH C:\xampp\htdocs\internship\resources\views/auth/login.blade.php ENDPATH**/ ?>
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\internship\resources\views/auth/login.blade.php ENDPATH**/ ?>
